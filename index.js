e90f0325aa7b08a449df59666d33bbe42e7516e7
mishangoti <mishan2512@gmail.com>
2023-06-20 15:41:24 -0400

    api tested deployed by 'sls deploy'

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./src/index');
+
+const server  = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+// module.exports.handler = async (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+};
